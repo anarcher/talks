cada7a3ef07da1e5d26f17d569961d308c1701f8
anarcher <anarcher@gmail.com>
2015-08-10 01:59:17 +0900

    example lrucache

--- 2015/gokit/lrucache/endpoint.go
+++ 2015/gokit/lrucache/endpoint.go
@@ -3,11 +3,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-kit/kit/endpoint"
 	"github.com/hashicorp/golang-lru"
 	"golang.org/x/net/context"
-	"log"
 )
+
+type AddRequest struct {
+	A, B int64
+}
+
+type AddResponse struct {
+	V int64
+}
 
 // START1 OMIT
 type CacheKeyFunc func(request interface{}) (interface{}, bool)
@@ -19,14 +27,55 @@ func NewLRUCache(cache *lru.Cache, cacheKey CacheKeyFunc) endpoint.Middleware {
 			if !ok {
 				return next(ctx, request)
 			}
-
-			val, ok := cache.Key(key)
-			if !ok {
-				return next(ctx, request)
+			val, ok := cache.Get(key)
+			if ok {
+				fmt.Println("Return from cache", request, val)
+				return val, nil
 			}
-			return val
+			val, err := next(ctx, request)
+			if err != nil {
+				return val, err
+			}
+			cache.Add(key, val)
+			fmt.Println("Return from endpoint", request, val)
+			return val, err
 		}
 	}
 }
 
 // STOP1 OMIT
+
+func main() {
+	// S:MAIN OMIT
+	cacheKeyFunc := func(request interface{}) (interface{}, bool) {
+		if req, ok := request.(AddRequest); ok {
+			return fmt.Sprintf("%d+%d", req.A, req.B), true
+		}
+		return nil, false
+	}
+
+	cache, _ := lru.New(10)
+	e := makeEndpoint()
+	e = NewLRUCache(cache, cacheKeyFunc)(e)
+
+	req := AddRequest{1, 2}
+	resp, err := e(context.Background(), req)
+	fmt.Println("resp", resp, "err", err)
+
+	resp, err = e(context.Background(), req)
+	fmt.Println("resp", resp, "err", err)
+
+	// E:MAIN OMIT
+
+}
+
+// S:makeEndpoint OMIT
+func makeEndpoint() endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		addReq := request.(AddRequest)
+		addRes := AddResponse{V: addReq.A + addReq.B}
+		return addRes, nil
+	}
+}
+
+// E:makeEndpoint OMIT
---
+++ 2015/gokit/package.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	_ "github.com/go-kit/kit/endpoint"
+	_ "github.com/hashicorp/golang-lru"
+	_ "golang.org/x/net/context"
+)
+
+func main() {
+}
