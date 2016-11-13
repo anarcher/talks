5a90e3295dd6d26358964b9e0997a02bd617c1fe
anarcher <anarcher@gmail.com>
2016-11-13 19:25:48 +0900

    Swarmkit routing-mesh and service discovery

---
+++ 2016/new-kits-on-the-block/run_nodes.sh
@@ -0,0 +1,8 @@
+SWARM_TOKEN=$(docker swarm join-token -q worker)
+SWARM_MASTER=$(docker info | grep -w 'Node Address' | awk '{print $3}')
+NUM_WORKERS=3 
+for i in $(seq "${NUM_WORKERS}"); do 
+	docker run -d --privileged --name worker-${i} \
+            --hostname=worker-${i} -p ${i}2375:2375 docker:1.12.1-dind \
+            dockerd-entrypoint.sh ; docker --host=localhost:${i}2375 swarm join --token ${SWARM_TOKEN} ${SWARM_MASTER}:2377
+done
