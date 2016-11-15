edeae4034e66d3d41c65f6e520638aed1f4acea3
msshin <msshin@gracenote.com>
2016-11-15 17:21:20 +0900

    Add docker-compose.yml (portatiner & visualizer)

--- 2016/new-kits-on-the-block/run_nodes.sh
+++ 2016/new-kits-on-the-block/run_nodes.sh
@@ -3,6 +3,6 @@ SWARM_MASTER=$(docker info | grep -w 'Node Address' | awk '{print $3}')
 NUM_WORKERS=3 
 for i in $(seq "${NUM_WORKERS}"); do 
 	docker run -d --privileged --name worker-${i} \
-            --hostname=worker-${i} -p ${i}2375:2375 docker:1.12.1-dind \
-            dockerd-entrypoint.sh ; docker --host=localhost:${i}2375 swarm join --token ${SWARM_TOKEN} ${SWARM_MASTER}:2377
+            --hostname=worker-${i} -p ${i}2375:2375 docker:1.12.3-dind
+    docker --host localhost:${i}2375 swarm join --token ${SWARM_TOKEN} ${SWARM_MASTER}:2377
 done
