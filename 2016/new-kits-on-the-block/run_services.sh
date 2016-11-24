bc1e3822faa6d24ee387d424c8ebc40232fb684b
anarcher <anarcher@gmail.com>
2016-11-24 17:58:07 +0900

    Changes slides (Add LB slide)

--- 2016/new-kits-on-the-block/run_services.sh
+++ 2016/new-kits-on-the-block/run_services.sh
@@ -12,3 +12,7 @@ docker service create --name nginx-2 --network my-net --endpoint-mode=dnsrr --mo
 
 docker service create --name nginx-3 --publish 8002:80 --network my-net --endpoint-mode=vip --mount type=bind,source=/etc/hosts,target=/usr/share/nginx/html/hosts nginx
 
+docker service create --mode global --constraint "node.role==worker" --endpoint-mode=dnsrr --name cadvisor --mount type=bind,source=/,target=/rootfs:ro --mount type=bind,source=/var/run,target=/var/run --mount type=bind,source=/sys,target=/sys --mount type=bind,source=/var/lib/docker,target=/var/lib/docker google/cadvisor
+
+
+
